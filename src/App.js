fcf23c2151d4dec1d700ec3f91d50c986a3b6030
Edgar <edgar.baudry@gmail.com>
2022-04-13 17:50:10 -0700

    added colors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { useEffect, useRef, useState, useLayoutEffect } from 'react';
 
-const mapWidth = 20;
-const mapHeight = 30;
+const mapWidth = 50;
+const mapHeight = 50;
 const gridRatio = 0.01;
 const maxPanToSelect = 10.0;
 const frameRate = 30;
-const colorNum = 5;
+const colorNum = 16;
 const selectionOverlaySize = 0.05;
 
 //and use window.innerWidth + window.innerHeight
@@ -122,12 +122,23 @@ function App() {
 
   const pickColor = (index) => {
     switch(index){
-      case 0: return "white";
-      case 1: return "blue";
-      case 2: return "red";
-      case 3: return "black";
-      case 4: return "pink";
-      default: return "black";
+      case 0: return "#f9ffff";//white
+      case 1: return "#9c9d97";//lgray
+      case 2: return "#474f52";//dgray
+      case 3: return "#1d1c21";//black
+      case 4: return "#ffd83d";//yellow
+      case 5: return "#f9801d";//orange
+      case 6: return "#b02e26";//red
+      case 7: return "#825432";//brown
+      case 8: return "#80c71f";//lime
+      case 9: return "#5d7c15";//green
+      case 10: return "#3ab3da";//lblue
+      case 11: return "#169c9d";//cyan
+      case 12: return "#3c44a9";//blue
+      case 13: return "#f38caa";//pink
+      case 14: return "#c64fbd";//magenta
+      case 15: return "#8932b7";//purple
+      default: return "#f9ffff";
     }
   }
 
